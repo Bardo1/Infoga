3bdf109dc33af27a7eaf2d4a7a8f14148f6760b7
m4ll0k <m4ll0k@users.noreply.github.com>
2017-03-29 23:16:20 +0200

    Add files via upload

--- infoga.py
+++ infoga.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*- 
 # infoga - Gathering Email Information Tool
-# by M0M0 (m4ll0k) - (c) 2017
-
+# Coded by M0M0 (m4ll0k)
 
 __license__ = """
 Copyright (c) 2017, {M0M0 (m4ll0k)}
@@ -32,227 +31,354 @@ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISE
 OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from lib import color,parser, info 
-from recon import *
-##
 try:
-	import os
+	from lib import color
+	from lib import parser
+	from lib import info
+	import os 
 	import sys 
-	import urllib3
+	import urllib3 
 	import requests
-	import getopt
 	import re
 	import socket
 	import json
+	import mechanize 
+	import getopt
 	from urlparse import urlparse
 	from time import strftime, sleep
-except ImportError as error:
-	print "\n{}[!] "+str(error)+"{}\n".format(color.incolor.RED,color.incolor.RESET)
-##
-##
-class infoga:
-	##
-	def __init__(self, argv):
+	from lxml.html import fromstring
+	from recon import * 
+except ImportError as err:
+	print "%s]![%s %s%s%s"%("\033[1;31m","\033[0m","\033[1;38m",str(err),"\033[0m")
+	sys.exit()
+
+
+class Infoga:
+	'''
+	Infoga - Gathering Email Information Tool
+	'''
+	def __init__(self,argv):
+		####### vars #######
+		self.n = info.__name__
+		self.i = info.__info__
+		self.v = info.__version__
+		self.c = info.__codename__
+		self.a = info.__author__
+		self.g = info.__giturl__
+		self.s = info.__site__
+		cc = color.colors()
+		self.red = cc.red()
+		self.green = cc.green()
+		self.yellow = cc.yellow()
+		self.white = cc.white()
+		self.blue = cc.blue()
+		self.cyan = cc.cyan()
+		self.end = cc.reset()
+		self.und = cc.ind()
+		self.nwhite = cc.nwhite()
 		self.argv = argv
-		self.allemails = []
-		self.conn = ""
-		self.strf = "[%s] "% (strftime('%H:%M:%S'))
-		# colors  
-		self.r = color.incolor.RED
-		self.y = color.incolor.YELLOW
-		self.w = color.incolor.WHITE
-		self.mw = color.incolor.MWHITE
-		self.t = color.incolor.RESET 
-		self.g = color.incolor.GREEN
-		self.b = color.incolor.BLUE
-		self.c = color.incolor.CRIMSON
-		self.n = color.incolor.CYAN
-		self.i = color.incolor.IND
-		# info
-		self.ver = info.__version__
-		self.name = info.__name__
-		self.desc = info.__info__
-		self.code = info.__codename__
-		self.auth = info.__author__
-		self.url = info.__giturl__
+		self.allemail = []
+		self.new = []
+		self.strf = "%s"%str((strftime('%H:%M:%S')))
+		self.sock = []
 
 	def banner(self):
-		# ----------------------------------------------------------------
-		print "{} ___        ___                    {}".format(self.r,self.t)
-		print "{}|   .-----.'  _.-----.-----.---.-. {}".format(self.r,self.t)
-		print "{}|.  |     |   _|  _  |  _  |  _  | {}".format(self.r,self.t)
-		print "{}|.  |__|__|__| |_____|___  |___._| {}".format(self.r,self.t)
-		print "{}|:  |                |_____|   {}{}\"{}\"{}".format(self.r,self.t,self.n,self.ver,self.t)
-		print "{}|::.|{}{} N4m3:{} {}{} - {}{}        ".format(self.r,self.t,self.w,self.t,self.y,self.name,self.desc,self.t)
-		print "{}|:..|{}{} C0d3n4m3:{} {}{}{}         ".format(self.r,self.t,self.w,self.t,self.y,self.code,self.t)
-		print "{}|...|{}{} 4uth0r:{} {}{}{}           ".format(self.r,self.t,self.w,self.t,self.y,self.auth,self.t)    
-		print "{}| - |{}{} Github:{} {}{}{}{}         ".format(self.r,self.t,self.w,self.t,self.i,self.y,self.url,self.t)
-		print "{}`---'{}                            \n".format(self.r,self.t)
+		print self.red+"\t __        ___                      "+self.end
+		print self.red+"\t|__.-----.'  _.-----.-----.---.-.   "+self.end
+		print self.red+"\t|  |     |   _|  _  |  _  |  _  |   "+self.end
+		print self.red+"\t|__|__|__|__| |_____|___  |___._|   "+self.end
+		print self.red+"\t                    |_____|         "+self.end
+		print self.yellow+"-- -=[%s %s%s %s - %s            %s"%(self.end,self.nwhite,self.n,self.v,self.i,self.end)
+		print self.yellow+"-- -=[%s %s%s - \"%s\"           %s"%(self.end,self.nwhite,self.n,self.c,self.end)
+		print self.yellow+"-- -=[%s %s%s - %s               %s"%(self.end,self.nwhite,self.a,self.s,self.end)
+		print self.yellow+"-- -=[%s %s%s                  %s\n"%(self.end,self.nwhite,self.g,self.end)
 
 	def usage(self):
-		# ------------------------------------------------------------------
 		path = os.path.basename(sys.argv[0])
 		self.banner()
-		print "{}Usage: {} -t [target] -s [source]:{}\n".format(self.mw,path,self.t)
-		print "{}\t-t\tDomain to search or company name{}".format(self.mw,self.t)
-		print "{}\t-s\tData source: all, google, bing, pgp, yahoo{}".format(self.mw,self.t)
-		print "{}\t-h\tShow this help and exit{}\n".format(self.mw,self.t)
-		print "{}Examples:{}".format(self.mw,self.t)
-		print "{}\t{} -t site.com -s all{}".format(self.mw,path,self.t)
-		print "{}\t{} -t site.com -s [google, bing, pgp, yahoo]{}\n".format(self.mw,path,self.t)
+		print "Usage: %s -t [target] -s [source]\n"% path
+		print "\t-t\t\tDomain to search"
+		print """\t-s\t\tData source: [all, google, yahoo, bing, \n\t\t\t\t\tpgp, yandex, baidu]"""
+		print "\t-i\t\tGet email informations"
+		print "\t--update\tUpdate tool"
+		print "\t--version\tShow version"
+		print "\t--help\t\tShow this help and exit\n"
+		print "Examples:"
+		print "\t %s -t http://www.site.com -s all"% path
+		print "\t %s -t site.com -s [google, bing, ...]"% path
+		print "\t %s -i email123@site.com"% path
+		print ""
+		sys.exit()
+
+	def pprint(self,string,flag="+"):
+		print '{}[{}]{}{}[{}]{} {}'.format(self.white,self.strf,self.end,self.green,str(flag),self.end,str(string))
+
+	def nprint(self,string,flag="-"):
+		print '{}[{}]{}{}[{}]{} {}'.format(self.white,self.strf,self.end,self.yellow,str(flag),self.end,str(string))
+	
+	def eprint(self,string,flag="!"):
+		print '{}[{}]{}{}[{}]{} {}'.format(self.white,self.strf,self.end,self.red,str(flag),self.end,str(string))
 
 	def google(self):
-		print "{}{}{}{}Searching \"{}\" in Google...{}".format(self.mw,self.strf,self.t,self.y,self.keyword,self.t)
+		self.pprint("Searching \"%s\" in Google..."%self.keyword)
 		search = googlesearch.google_search(self.keyword)
 		search.process()
-		self.allemails = search.get_emails()
+		all_email = search.get_emails()
+		self.allemail.extend(all_email)
 
 	def bing(self):
-		print "{}{}{}{}Searching \"{}\" in Bing...{}".format(self.mw,self.strf,self.t,self.y,self.keyword,self.t)
+		self.pprint("Searching \"%s\" in Bing..."%self.keyword)
 		search = bingsearch.bing_search(self.keyword)
 		search.process()
-		self.allemails = search.get_emails()
+		all_email = search.get_emails()
+		self.allemail.extend(all_email)
 
 	def yahoo(self):
-		print "{}{}{}{}Searching \"{}\" in Yahoo...{}".format(self.mw,self.strf,self.t,self.y,self.keyword,self.t)
+		self.pprint("Searching \"%s\" in Yahoo..."%self.keyword)
 		search = yahoosearch.yahoo_search(self.keyword)
 		search.process()
-		self.allemails = search.get_emails()
+		all_email = search.get_emails()
+		self.allemail.extend(all_email)
 
 	def pgp(self):
-		print "{}{}{}{}Searching \"{}\" in PGP...{}".format(self.mw,self.strf,self.t,self.y,self.keyword,self.t)
+		self.pprint("Searching \"%s\" in PGP..."%self.keyword)
 		search = pgpsearch.pgp_search(self.keyword)
 		search.process()
-		self.allemails = search.get_emails()
+		all_email = search.get_emails()
+		self.allemail.extend(all_email)
+
+	def yandex(self):
+		self.pprint("Searching \"%s\" in Yandex..."%self.keyword)
+		search = yandexsearch.yandex_search(self.keyword)
+		search.process()
+		all_email = search.get_emails()
+		self.allemail.extend(all_email)
+
+	def baidu(self):
+		self.pprint("Searching \"%s\" in Baidu..."%self.keyword)
+		search = baidusearch.baidu_search(self.keyword)
+		search.process()
+		all_email = search.get_emails()
+		self.allemail.extend(all_email)
 
 	def all(self):
 		self.google()
 		self.bing()
 		self.yahoo()
 		self.pgp()
-		self.allemails.extend(self.allemails)
-		self.allemails = sorted(set(self.allemails))
+		self.yandex()
+		self.baidu()
+	
+	def checkemail(self,email):
+		email = self.email
+		if '@' in email:
+			allemail = str(email)
+			data = {'lang':'en'}
+			data['email'] = allemail
+			req = requests.post('http://www.mailtester.com/testmail.php',data=data)
+			pattern = re.compile(r'[0-9]+(?:\.[0-9]+){3}')
+			findip = pattern.findall(req.content)
+			for q in findip:
+				if q not in self.new:
+					self.new.append(q)
+			tree = fromstring(req.text)
+			tree.forms[0].getparent().remove(tree.forms[0])
+			msg_list = tree.xpath('//table[last()]/tr[last()]/td[last()]/text()')
+			msg = ' '.join([x.strip() for x in msg_list])
+			
+			if 'is valid' in msg:
+				print ""
+				self.pprint("{}Email:{} {}{}{} ({}{}{})".format(self.blue,self.end,self.yellow,allemail,self.end,self.green,msg,self.end))
+			else:
+				print ""
+				self.pprint("{}Email:{} {}{}{} ({}{}{})".format(self.blue,self.end,self.yellow,allemail,self.end,self.red,msg,self.end))
+			
+			for s in range(len(self.new)):
+				net = urllib3.PoolManager()
+				res = net.request('GET',"https://api.shodan.io/shodan/host/"+self.new[s]+\
+					"?key=UNmOjxeFS2mPA3kmzm1sZwC0XjaTTksy")
+				self.jso = json.loads(res.data)
 
-	def queque(self, findip):
-		self.new = []
-		for q in self.findip:
-			if q not in self.new:
-				self.new.append(q)
-		return self.new
-	def socket(self):
-		v = '\n'.join(self.new)
-		self.conn = socket.gethostbyaddr(v)
-		return self.conn[0]
+				if 'country_code' and 'country_name' in self.jso:
+					print ""
+					print "\t\t|_ {}{}{}".format(self.green,self.new[s],self.end)
+					print "\t\t\t|"
+					print "\t\t\t|__ Country: {}({}) - City: {} ({})".format(self.jso['country_code'],self.jso['country_name'],\
+						self.jso['city'],self.jso['region_code'])
+					print "\t\t\t|__ ASN: {} - ISP: {}".format(self.jso['asn'],self.jso['isp'])
+					print "\t\t\t|__ Latitude: {} - Longitude: {}".format(self.jso['latitude'],self.jso['longitude'])
+					print "\t\t\t|__ Hostname: {} - Organization: {}".format(self.jso['hostnames'],self.jso['org'])
+					print ""
+
+				elif 'No information available for that IP.' or 'error' in jso:
+					print "\t\t|__ {}{}{}".format(self.green,self.new[s],self.end)
+					print "\t\t\t|__{}No information available for that IP!!{}".format(self.red,self.end)
+					print ""
+
+				else:
+					print "\t\t|__ {} ({})".format(str(self.new[s]))
+			sys.exit()
+		else:
+			print ""
+			self.eprint("{}Check your email... :){}".format(self.red,self.end))
+			print ""
+			sys.exit()
+
+	def checkurl(self):
+		o = urlparse(self.keyword)
+		scheme = o.scheme
+		netloc = o.netloc
+		path = o.path
+
+		if scheme not in ['http','https','']:
+			print ""
+			self.eprint("{}Scheme {} not supported!!{}".format(self.red,scheme,self.end))
+			print ""
+			sys.exit()
+
+		if netloc == '':
+			url = str(path)
+		else:
+			url = str(netloc)
+		self.keyword = url
+
+	def get_info(self):
+		for x in xrange(len(self.allemail)):
+			data = {'lang':'en'}
+			data['email'] = self.allemail[x]
+			req = requests.post('http://www.mailtester.com/testmail.php',data=data)
+			pattern = re.compile(r'[0-9]+(?:\.[0-9]+){3}')
+			findip = pattern.findall(req.content)
+			for q in findip:
+				if q not in self.new:
+					self.new.append(q)
+
+			self.pprint("{}Email:{} {}{}{}".format(self.blue,self.end,self.yellow,self.allemail[x],self.end))
+			
+			for s in range(len(self.new)):
+				#########################
+				net = urllib3.PoolManager()
+				res = net.request('GET',"https://api.shodan.io/shodan/host/"+self.new[s]+\
+					"?key=UNmOjxeFS2mPA3kmzm1sZwC0XjaTTksy")
+				self.jso = json.loads(res.data,'utf-8')
+				try:
+					v = "\n".join(self.new)
+					self.sock = socket.gethostbyaddr(v)
+				except socket.herror:
+					self.sock = ['%s'% self.jso['hostnames']]
+
+				if 'country_code' and 'country_name' in self.jso:
+					print "\t\t|_ {}{}{} ({})".format(self.green,self.new[s],self.end,self.sock[0])
+					print "\t\t\t|"
+					print "\t\t\t|__ Country: {}({}) - City: {} ({})".format(self.jso['country_code'],self.jso['country_name'],\
+						self.jso['city'],self.jso['region_code'])
+					print "\t\t\t|__ ASN: {} - ISP: {}".format(self.jso['asn'],self.jso['isp'])
+					print "\t\t\t|__ Latitude: {} - Longitude: {}".format(self.jso['latitude'],self.jso['longitude'])
+					print "\t\t\t|__ Hostname: {} - Organization: {}".format(self.jso['hostnames'],self.jso['org'])
+					print ""
+
+				elif 'No information available for that IP.' or 'error' in jso:
+					print 
+					print "\t\t|__ {}{}{} ({})".format(self.green,self.new[s],self.end,self.sock[0])
+					print "\t\t\t|\t|__{}No information available for that IP!!{}".format(self.red,self.end)
+					print ""
+
+				else:
+					print "\t\t|__ {} ({})".format(self.green,self.new[s],self.end,self.sock[0])
+
+	def checkversion(self):
+		print ""
+		self.pprint("%s%s%s %s%s%s: %s%s%s"% (self.red,self.n,self.end,self.blue,self.v,self.end,self.white,self.i,self.end))
+		print ""
+		sys.exit(0)
+
+	def checkupdate(self):
+		path = os.getcwd()
+		listpath = os.listdir(path)
+		if ".git" not in listpath:
+			print ""
+			self.eprint("{}Git directory not found, please download Infoga from Github repository{}".format(self.red,self.end))
+			print ""
+			sys.exit()
+		elif ".git" in listpath:
+			print ""
+			self.pprint("{}Updateting {}...{}".format(self.yellow,self.n,self.end))
+			os.system('git pull')
+			sys.exit()
 
 	def start(self):
-		# ------------------------------------------------------------------
-		if len(sys.argv) < 4:
+		if len(sys.argv) < 2:
 			self.usage()
-			sys.exit(0)
 		try:
-			opts,args = getopt.getopt(self.argv, "t:s:h")
+			opts,args = getopt.getopt(self.argv, "t:s:i:h:vu:",["update","version","help"])
 		except getopt.GetoptError:
 			self.usage()
-			sys.exit(0)
 
 		for opt,arg in opts:
-			if opt == "-t":
+			if opt == '-t':
 				self.keyword = arg
-			elif opt == "-h":
-				self.usage()
-				sys.exit(0)
-			elif opt == "-s":
+				self.checkurl()
+			elif opt == '-s':
 				self.engine = arg
-				if self.engine not in ("all, google, bing, pgp, yahoo"):
-					print "\n{}[!]{}{} Invalid search engine!! Try with: all, google, bing, yahoo or pgp.{}\n".format(self.r,self.t,self.w,self.t)
-					sleep(1)
-					self.usage()
-					sys.exit(0)
-				else:
-					pass
-
-		o = urlparse(self.keyword)
-		if o[0] in ['http','https']:
-			print "\n{}[!]{}{} Invalid scheme!! Try without: http://, https:// and www. :){}\n".format(self.r,self.t,self.w,self.t)
-			sleep(1)
-			sys.exit(0)
-		else:
-			pass
-
-		###############################
-		################################
+				if self.engine not in ("all","google","bing","yahoo","pgp","baidu","yandex"):
+					print ""
+					self.eprint("{}Invalid search engine!! Try with: all,google,bing,yahoo,pgp,baidu,yandex{}".format(self.red,self.end))
+					print ""
+					sys.exit()
+			elif opt == '-i':
+				self.email = arg
+				self.checkemail(self.email)
+			elif opt in ('-h','--help'):
+				self.usage()
+			elif opt in ('-v','--version'):
+				self.checkversion()
+			elif opt in ('-u','--update'):
+				self.checkupdate()
 
 		if self.engine == "google":
 			self.banner()
 			self.google()
-			######################
+
 		elif self.engine == "bing":
 			self.banner()
 			self.bing()
-			######################
+
 		elif self.engine == "yahoo":
 			self.banner()
 			self.yahoo()
-			######################
+		
 		elif self.engine == "pgp":
 			self.banner()
 			self.pgp()
-			######################
+
+		elif self.engine == "yandex":
+			self.banner()
+			self.yandex()
+
+		elif self.engine == "baidu":
+			self.banner()
+			self.baidu()
+
 		elif self.engine == "all":
 			self.banner()
 			self.all()
-			#######################
-			#######################
-		if self.allemails == []:
-			print "\n{}{}{}{}Not found emails!!{}\n".format(self.mw,self.strf,self.t,self.r,self.t)
-			sys.exit(0)
-			##
+
+		if self.allemail == []:
+			self.nprint("Not found email!!\n")
+			sys.exit()
 		else:
-			print "\n{}{}{}{}All Email Found: {}\n".format(self.mw,self.strf,self.t,self.y,self.t)
-			for x in xrange(len(self.allemails)):
-				data = {'lang':'en'}
-				data['email'] = self.allemails[x]
-				req = requests.post('http://www.mailtester.com/testmail.php', data=data)
-				regex = re.compile(r"[0-9]+(?:\.[0-9]+){3}")
-				self.findip = regex.findall(req.content)
-				self.queque(self.findip)
-				print "{}Email: {}{}".format(self.r,self.t,self.allemails[x])
-				# ----
-				for s in range(len(self.new)):
-					net = urllib3.PoolManager()
-					res = net.request('GET', "https://api.shodan.io/shodan/host/"+self.new[s]+\
-						"?key=UNmOjxeFS2mPA3kmzm1sZwC0XjaTTksy")
-					self.jso = json.loads(res.data)
-
-				if 'country_code' and 'country_name' in self.jso:
-					for c in range(len(self.new)):
-						print "\t\t|_ {}{}{} ({})".format(self.g,self.new[s],self.t,self.socket())
-						print "\t\t\t|"
-						print "\t\t\t|__ Country: {}({}) - City: {} ({})".format(self.jso['country_code'],self.jso['country_name'],\
-							self.jso['city'],self.jso['region_code'])
-						print "\t\t\t|__ ASN: {} - ISP: {}".format(self.jso['asn'],self.jso['isp'])
-						print "\t\t\t|__ Latitude: {} - Longitude: {}".format(self.jso['latitude'],self.jso['longitude'])
-						print "\t\t\t|__ Hostname: {} - Organization: {}".format(self.jso['hostnames'],self.jso['org'])
-						print ""
-
-				elif 'No information available for that IP.' or 'error' in self.jso:
-					print "\t\t\t|__ {} ({})"% (self.new[s],self.socket())
-					print "\t\t\t|\t|__{}No information available for that IP!!{}".format(self.r,self.t)
-					print ""
-				
-				else:
-					print "\t\t\t|__ {} ({})".format(self.new[s],self.socket())
+			self.allemail = sorted(set(self.allemail))
+			self.pprint("All email found: ")
+			self.get_info()
 
 def main(argv):
-	main = infoga(argv)
+	main = Infoga(argv)
 	main.start()
 
 if __name__ == "__main__":
 	try:
 		main(sys.argv[1:])
 	except KeyboardInterrupt as err:
-		print "\n{}[!] Ctrl+c.. :({}".format(color.incolor.RED,color.incolor.RESET)
-		sleep(0.5)
+		print "{}Ctrl+c... Sn4duu :){}"%("\033[1;31m","\033[0m")
 		sys.exit(0)
 
-		
